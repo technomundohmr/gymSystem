b3de5aee325eaf836323886f197d6597004545d0
AndresR <technomundohmr@gmail.com>
2024-04-03 15:08:41 -0500

    Ajustes de validaciones e idioma

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
+use App\Models\Roles;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\Passport;
@@ -26,8 +29,69 @@ class AuthController extends Controller
     /**
      * use register method
      */
-    public function register(Request $request) {
-        dump($request->getContent());
+    public function register(UserRequest $request) {
+        $register_data = $request->validated();
+        dump($register_data);
         die;
+        if(!empty($register_data['id_number'] && $register_data['tipo_id'])) {
+            $register_data['full_id'] = $register_data['tipo_id'] . '-' . $register_data['id_number'];
+        } else {
+            return response()->json(['message' => 'los campos de identificación no existen']);
+        }
+
+        if(!empty($register_data['password'])) {
+            $register_data['password'] = bcrypt( $register_data['password']);
+        }
+
+        if(!empty($register_data['role_id'])) {
+            $role_name = $register_data['role_id'];
+            $role = Roles::where('machine_id', '=', $register_data['role_id'] )->first();
+            if(!empty($role)){
+                $register_data['role_id'] = $role->getAttribute('id');
+            } else{
+                return response()->json(['message' => 'there was an error: El rol seleccionado no existe']);
+            }
+
+            switch ($role_name) {
+                case 'gym':
+                    $user = $this->_create_gym($register_data);
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+        }
+
+        if(empty($user['error'])) {
+            $token = $user->createToken('superadminToken')->plainTextToken;
+            return response()->json([
+                'message' => 'user created succesfully',
+                'token' => $token
+            ]);
+        }else {
+            return response()->json($user);
+        }
+
+    }
+
+    public function _create_gym($data) {
+        //@todo in frontend ask owner if want to register same gym data 
+        try {
+            $user = User::create($data);
+            return $user;
+        } catch (\Throwable $th) {
+            return ['error' => $th];
+        }
+    }
+
+    public function _create_owner($data) {
+        //@todo in frontend ask owner if want to register same gym data 
+        try {
+            $user = User::create($data);
+            return $user;
+        } catch (\Throwable $th) {
+            return ['error' => $th];
+        }
     }
 }
---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|Unique:users',
+        ];
+        
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json($validator->errors(), JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'el nombre es requerido',
+        ];
+    }
+}
